42f2aa8af8592b6c8ce6e68d68bf741acbfb5d7a
Mar0xy <marie@mxy.gg>
2021-08-23 21:01:12 +0200

    Create queue command

---
+++ commands/queue.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'Queue',
+    description: 'Show Queued items.',
+    hide: false,
+    perms: [],
+    args: false,
+    async execute(message, args) {
+        
+    }
+}
