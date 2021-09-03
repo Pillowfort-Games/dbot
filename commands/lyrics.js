9f6f37d678b26d9a291259e65e160fc2d79b0424
Mar0xy <marie@mxy.gg>
2021-09-03 23:19:29 +0200

    Lyrics Update (Unreliable until fix)

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,4 +1,4 @@
-const lyricsParse = require('lyrics-parse');
+const findLyrics = require('@j0r6it0/lyricsfinder');
 const { MessageEmbed } = require('discord.js');
 module.exports = {
     name: 'Lyrics',
@@ -10,12 +10,13 @@ module.exports = {
         const { queue } = await import('../sublist.mjs');
         let active = queue.find(queue => queue.active === true);
         if (active) {
-            lyricsParse(active.queued[0].name).then(async lyrics => {
-                if (lyrics) {
+            findLyrics(active.queued[0].name).then(async lyrics => {
+                const lyric = lyrics;
+                if (lyric) {
                     const embi = new MessageEmbed()
                         .setColor('#A30DAC')
-                        .setDescription(lyrics)
-                        .setFooter('Provided by Google', 'https://www.google.com/images/branding/googleg/1x/googleg_standard_color_128dp.png');
+                        .setDescription(lyric)
+                        .setFooter('Provided by Genius/Google', 'https://assets.genius.com/images/default-api-app-image.png');
                     message.channel.send({ embeds: [embi] });
                 } else {
                     const embi = new MessageEmbed()
