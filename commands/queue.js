693b0b49cf7ed1407ed3ccaf30338aee9341dacd
Mar0xy <marie@mxy.gg>
2021-09-06 01:22:58 +0200

    Make queue command functional

--- commands/queue.js
+++ commands/queue.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
     name: 'Queue',
     description: 'Show Queued items.',
@@ -8,7 +10,24 @@ module.exports = {
         const { queue } = await import('../sublist.mjs');
         let active = queue.find(queue => queue.active === true);
         if (active) {
-            
+            let desc = '';
+            const embi = new MessageEmbed()
+                .setTitle('Next 5 videos in queue')
+                .setColor('#A30DAC');
+            const embi2 = new MessageEmbed()
+                .setTitle('Currently Playing')
+                .setColor('#169E54');
+
+            for (let num = 0; num != 5; num++) {
+                if (num === 0) {
+                    embi2.setDescription(`[${active.queued[num].name}](${active.queued[num].url})\nRequested by ${active.queued[num].requester}`);
+                } else if(num !== 0 && active.queued.length > 1) {
+                    desc += `${num}. [${active.queued[num].name}](${active.queued[num].url})\nRequested by ${active.queued[num].requester}\n`;
+                }
+            }
+            embi.setDescription(desc);
+            message.channel.send({ embeds: [embi2] });
+            active.queued.length > 1 ? message.channel.send({ embeds: [embi] }) : '';
         }
     }
 }
